56c97fa03175a1eedd39cdd1a0869497c99f2af8
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-03 06:45:42 +0200

    kinda fixed player movment

--- Player.gd
+++ Player.gd
@@ -5,12 +5,15 @@ const UP = Vector2(0, -1)
 const SPEED = 60
 const GRAVITY = 260
 const JUMP_SPEED = -180
+const FLOOR_TIMER = 0.1
 
 var movement = Vector2()
 var last_anim = 0
+var on_floor_timer = 0
 
 func _ready():
 	$Sprite.playing = true
+	set_safe_margin(0.01)
 	
 func set_anim(anim):
 	if anim == last_anim:
@@ -28,35 +31,54 @@ func set_anim(anim):
 		$Sprite.animation = "Fall"
 	
 func _physics_process(delta):
+	var on_floor = on_floor_timer > 0
+	
+	if on_floor:
+		movement.y = 0
+	
 	movement.y += GRAVITY * delta
+	
+	var moved = false
+	
 	if Input.is_action_pressed("ui_left"):
 		movement.x = -SPEED
 		$Sprite.flip_h = true
-		if is_on_floor():
-			set_anim(2)
+		if on_floor:
+			moved = true
 	elif Input.is_action_pressed("ui_right"):
 		movement.x = SPEED
 		$Sprite.flip_h = false
-		if is_on_floor():
-			set_anim(2)
-	elif Input.is_action_pressed("ui_down") and is_on_floor():
+		if on_floor:
+			moved = true
+	else:
+		movement.x = 0
+	
+	if Input.is_action_pressed("ui_down") and on_floor:
 		set_anim(3)
 	else:
-		if is_on_floor():
-			set_anim(1)
-		if movement.y < 0:
-			set_anim(4)
+		if on_floor:
+			if moved:
+				set_anim(2)
+			else:
+				set_anim(1)
 		else:
-			set_anim(5)
+			if movement.y < 0:
+				set_anim(4)
+			else:
+				set_anim(5)
 	
-	if is_on_floor():
+	if on_floor:
 		if Input.is_action_just_pressed("ui_up"):
 			movement.y = JUMP_SPEED
-		else:
-			movement.y = 0
-		
-	movement = move_and_slide(movement, UP)
+			on_floor_timer = -1
 	
+	var new_movement = move_and_slide(movement, UP, 2)
+	movement.x = new_movement.x
+	if is_on_floor():
+		on_floor_timer = FLOOR_TIMER
+	else:
+		if on_floor_timer > -1:
+			on_floor_timer -= delta
 	
-	
+	$Label.text = "  %3.1f  %3.1f %3.1f" % [on_floor_timer, movement.y, get_safe_margin()]
 	
